f6cec2591c4eb567bca4826a0d4c01bc639a0868
EduardoPetrini <eduardo.petrini@outlook.com>
2022-11-28 08:57:59 -0300

    feat: search bar - fix remove entry

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import Form from "./components/Form";
 import ViewEntries from "./components/ViewEntries";
-import "./App.css"
+import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [entries, setEntries] = useState([]);
 
   const saveFormEntry = (entry) => {
-    setEntries((current) => [...current, entry]);
-  }
+    const id = Math.round(Math.random() * 100000);
 
-  const removeEntry = (index) => {
-    setEntries(current => {
-      return [...current.slice(0, index), ...current.slice(index + 1)]
-    })
-  }
+    console.log(`Add: ${id}`);
+    setEntries((current) => [...current, { ...entry, id }]);
+  };
+
+  const removeEntry = (id) => {
+    console.log(`Remove: ${id}`);
+    setEntries((current) => {
+      const newEntries = current.reduce((newEntries, entry) => {
+        if (entry.id !== id) {
+          return [...newEntries, entry];
+        }
+
+        return newEntries;
+      }, []);
+
+      return newEntries;
+    });
+  };
   return (
     <div id="simple-form">
       <Form saveFormEntry={saveFormEntry}></Form>
@@ -25,3 +37,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/ViewEntries.js
+++ src/components/ViewEntries.js
@@ -2,24 +2,24 @@ import { useState } from "react";
 
 function ViewEntries(props) {
   const { entries = [], removeEntry } = props;
-  const [ query, setQuery ] = useState("");
+  const [query, setQuery] = useState("");
 
-  const filteredEntries = entries.filter(({firstName, lastName, email}) =>{
+  const filteredEntries = entries.filter(({ firstName, lastName, email }) => {
     const singleString = `${firstName} ${lastName} ${email}`;
 
-    return singleString.includes(query)
-  })
+    return singleString.includes(query);
+  });
 
-  const clickRemoveEntry = (e, index) => {
-    removeEntry(index);
-  }
+  const clickRemoveEntry = (e, id) => {
+    removeEntry(id);
+  };
 
   return (
     <div>
       <p>Number of entries: {entries.length}</p>
       <div hidden={!entries.length ? true : false}>
-        <input type="search" placeholder="Search..." onChange={(e) => setQuery(e.target.value)}/>
-        <table >
+        <input type="search" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} />
+        <table>
           <thead>
             <tr>
               <th></th>
@@ -36,7 +36,9 @@ function ViewEntries(props) {
                 <td>{entry.firstName}</td>
                 <td>{entry.lastName}</td>
                 <td>{entry.email}</td>
-                <td className="remove" onClick={(e) => clickRemoveEntry(e, index)}>X</td>
+                <td className="remove" onClick={(e) => clickRemoveEntry(e, entry.id)}>
+                  X
+                </td>
               </tr>
             ))}
           </tbody>
